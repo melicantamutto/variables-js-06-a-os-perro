a3011592e2e886ed41be319907cd05b9857bc41d
MELI CANTAMUTTO <MELICANTAMUTTO@gmail.com>
2020-11-21 12:54:12 -0300

    ejercicio js

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('Años perro')
+
+alert('Tu edad en años perro')
+
+const edad = prompt('Ingresa tu edad');
+const exponente = (edad - 31)/16;
+const edadPerro = Math.exp(exponente);
+
+alert(`En años perro tenes ${edadPerro} años.`);
